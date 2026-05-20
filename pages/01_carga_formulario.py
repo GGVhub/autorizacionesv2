54c2553896ccc17f44ec398ede339adb2a42ad84
GGVhub <gus.vilche@gmail.com>
2026-05-20 07:54:10 -0300

    Update 01_carga_formulario.py

--- pages/01_carga_formulario.py
+++ pages/01_carga_formulario.py
@@ -15,7 +15,7 @@ if "formulario_guardado" not in st.session_state:
     st.session_state.formulario_guardado = False
 require_page_access("formulario")
 
-FECHA_LIMITE = datetime(2026, 5, 6, 8, 00, 0, tzinfo=timezone(timedelta(hours=-3)))
+FECHA_LIMITE = datetime(2026, 5, 23, 8, 00, 0, tzinfo=timezone(timedelta(hours=-3)))
 ahora = datetime.now(timezone(timedelta(hours=-3)))
 
 if ahora > FECHA_LIMITE:
